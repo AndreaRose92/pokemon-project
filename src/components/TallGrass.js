d75e982affdbcd000d483e04aecb37c082262a76
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-03 14:27:21 -0700

    why do i never add first

---
+++ src/components/TallGrass.js
@@ -0,0 +1,13 @@
+
+import PokemonCard from "./PokemonCard"
+
+export default function TallGrass({wildPokemon}) {
+
+    const renderMons = wildPokemon.length === 3 ? wildPokemon.map(pokemon => <PokemonCard key={pokemon.id} {...pokemon}/>) : null
+    
+    return (
+        <div>
+            {renderMons}
+        </div>
+    )
+}
